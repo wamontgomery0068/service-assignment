e27d50522748ad295a935409eba8c206c712e266
Your Name <wamontgomery0068@gmail.com>
2019-10-08 09:54:34 -0600

    Business Logic Implemented

--- src/app/counter.service.ts
+++ src/app/counter.service.ts
@@ -1,9 +1,27 @@
-import { Injectable } from '@angular/core';
-
-@Injectable({
-  providedIn: 'root'
-})
 export class CounterService {
 
-  constructor() { }
+  // Create an activeToInactiveCounter variable and set to 0
+  activeToInactiveCounter = 0;
+  // Create an inactiveToActiveCounter variable and set to 0
+  inactiveToActiveCounter = 0;
+
+  // Create an incrementActiveToInactive method
+  incrementActiveToInactive() {
+
+    // Everytime incrementActiveToInactive is initiated it will be incremented by 1
+    this.activeToInactiveCounter++;
+    // Send message to the console to indicate how many times activeToInactiveCounter has be incremented
+    console.log('Active to Inactive: ' + this.activeToInactiveCounter);
+    
+  }
+
+  // Create an incrementInactiveToActive method
+  incrementInactiveToActive() {
+
+    // Everytime incrementInactiveToActive is initiated it will be incremented by 1
+    this.inactiveToActiveCounter++;
+    // Send message to the console to indicate how many times inactiveToActiveCounter has be incremented
+    console.log('Inactive to Active ' + this.inactiveToActiveCounter);
+
+  }
 }
