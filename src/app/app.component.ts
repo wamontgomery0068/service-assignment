8584c9315cab34bdf9094de9de6ac09fe094a343
Your Name <wamontgomery0068@gmail.com>
2019-10-08 10:35:04 -0600

    Imported  UserService

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,9 +1,12 @@
 import { Component } from '@angular/core';
 
+import { UserService } from './users.service';
+
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
+  providers: [UserService]
 })
 export class AppComponent {
   title = 'service-assignment';
