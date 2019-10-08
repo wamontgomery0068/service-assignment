81c97c78bd71919f5b38f79416cec8a9bd81463e
Your Name <wamontgomery0068@gmail.com>
2019-10-08 10:18:28 -0600

    Business Logic Implemented

--- src/app/active-users/active-users.component.ts
+++ src/app/active-users/active-users.component.ts
@@ -1,15 +1,27 @@
 import { Component, OnInit } from '@angular/core';
 
+import { UserService } from '../users.service';
+
 @Component({
   selector: 'app-active-users',
   templateUrl: './active-users.component.html',
   styleUrls: ['./active-users.component.css']
 })
+
 export class ActiveUsersComponent implements OnInit {
 
-  constructor() { }
+  // Create an users array and set type to string
+  users: string [];
+
+  constructor(private userService: UserService) { }
 
   ngOnInit() {
+    this.users = this.userService.activeUsers;
+  }
+
+  // Create an onSetToInactive method
+  onSetToInactive(id: number) {
+    this.userService.setToInactive(id);
   }
 
 }
