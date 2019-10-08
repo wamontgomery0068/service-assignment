9a1f9bb86c1956cc723776ec455702a4f0492a1c
Your Name <wamontgomery0068@gmail.com>
2019-10-08 08:31:43 -0600

    Created InactiveUsersComponent

---
+++ src/app/inactive-users/inactive-users.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-inactive-users',
+  templateUrl: './inactive-users.component.html',
+  styleUrls: ['./inactive-users.component.css']
+})
+export class InactiveUsersComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
